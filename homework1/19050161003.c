fa75ba446dbd416116dd8879625035cbdf7bf3bd
Furkan Ekiz <96376500+furkanekiz@users.noreply.github.com>
2023-04-03 23:27:04 +0300

    Update 19050161003.c

--- homework1/19050161003.c
+++ homework1/19050161003.c
@@ -1,13 +1,26 @@
+// Furkan Ekiz - 19050161003
+// CENG342 - Homework3
+
+#include <mpi.h>
 #include <hellomake.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
 
-   int rows = atoi(argv[1]);
-   int cols = atoi(argv[2]);
+   int rank, numproc;
+
+   int matrixSize1 = atoi(argv[1]);
+   int matrixSize2 = atoi(argv[2]);
    char *filename = argv[3];
+   
+   MPI_Init(NULL, NULL);
+   
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   MPI_Comm_size(MPI_COMM_WORLD, &numproc);
+   
+   //matrixVectorMultiply(matrixSize1, rank, numproc, filename);
+   matrixVectorMultiply(matrixSize2, rank, numproc, filename);
 
-   matrixVectorMultiply(rows, cols, filename);
-
+   MPI_Finalize();
    return(0);
 }
